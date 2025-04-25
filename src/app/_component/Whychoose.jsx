076aef5c3cd5592b7901d22dcb4b3fa9e7034339
riteshk-007 <codeshorts007@gmail.com>
2025-04-25 13:47:50 +0530

    Implement code changes to enhance functionality and improve performance

--- src/app/_component/Whychoose.jsx
+++ src/app/_component/Whychoose.jsx
@@ -6,15 +6,14 @@ import Image from "next/image";
 export default function WhyChooseUs() {
   return (
     <section className="bg-gray-100">
-
       <div className="flex flex-col lg:flex-row items-start">
         <div className="w-full lg:w-1/2">
           <Image
-            src="/msn.webp"
+            src="/msn.jpeg"
             alt="Why choose KAAL TOOLS"
             width={500}
-            height={300} 
-            className="w-full md:h-96 object-cover"
+            height={300}
+            className="w-full h-auto object-cover"
             priority
           />
         </div>
@@ -22,28 +21,29 @@ export default function WhyChooseUs() {
         <div className="w-full lg:w-1/2 lg:pl-8 p-10">
           <div>
             <h3 className="text-4xl font-bold text-gray-900 mb-4">
-              WHY KAAL
+              WHY <span className="nd-logos text-[28px]">KAAL</span><sup>®</sup>
             </h3>
             <p className="text-gray-700 text-lg leading-relaxed mb-4">
-              At Kaal, we are committed to providing tools that not only meet but exceed project standards. Our dedication to quality ensures that every product we offer is built to last, perform under all conditions, and integrate the most advanced technology available.
+              <span className="nd-logos text-[14px]">KAAL</span><sup>®</sup> is committed to builders and maintainers - we understand the value of
+              time and our tools are made to be efficient and deliver precisely what you
+              need them for.
             </p>
-            <ul className="space-y-3">
-              <li className="text-gray-700">
-                Exceeds industry standards with innovative designs
-              </li>
-              <li className="text-gray-700">
-                Built for durability and exceptional performance in all conditions
-              </li>
-              <li className="text-gray-700">
-                Incorporates cutting-edge technology for maximum efficiency
-              </li>
-              <li className="text-gray-700">
-                Exceeds industry standards with innovative designs
-              </li>
-              <li className="text-gray-700">
-                Built for durability and exceptional performance in all conditions
-              </li>
-            </ul>
+            <p className="text-gray-700 text-lg leading-relaxed mb-4">
+              We are continuously developing new products and expanding our
+              comprehensive range of tools to ensure you have the right solution for every
+              task at hand.
+            </p>
+            <p className="text-gray-700 text-lg leading-relaxed mb-4">
+              Each and every product shipped goes through quality check before dispatch.
+              We offer customised services as well to meet specific demands for inspection
+              procedures and reports, through to unique packaging (other than our
+              standard), barcoding or product labelling, laser-marking for point of sale
+              presentation.
+            </p>
+            <p className="text-gray-700 text-lg leading-relaxed mb-4">
+              With <span className="nd-logos text-[14px]">KAAL</span><sup>®</sup>, you have a manufacturing partner you can rely on - every step of
+              the way!!!
+            </p>
           </div>
         </div>
       </div>
