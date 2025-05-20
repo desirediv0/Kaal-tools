053311858b22eafd39c73511558773b5b5ca44f0
riteshk-007 <codeshorts007@gmail.com>
2025-05-20 16:34:39 +0530

    fix: adjust hero banner layout by modifying column spans for better responsiveness

--- src/app/_component/hero-banner.jsx
+++ src/app/_component/hero-banner.jsx
@@ -54,7 +54,7 @@ export function HeroBanner({ items }) {
             {items.map((item, index) => (
               <CarouselItem key={index} className="h-full">
                 <div className="flex flex-col lg:grid lg:grid-cols-12 h-full items-center pt-8 xl:pr-2 xl:pl-6 pb-8 md:pb-12 lg:pb-0">
-                  <div className="w-full lg:col-span-3 flex flex-col sm:mt-10 xl:mt-[-80px]   text-white order-1 mb-2  lg:mb-0 text-center lg:text-start">
+                  <div className="w-full lg:col-span-4 xl:col-span-3 flex flex-col sm:mt-10 xl:mt-[-80px] text-white order-1 mb-2 lg:mb-0 text-center lg:text-start">
                     <h1 className="text-4xl lg:text-5xl font-semibold text-shadow uppercase break-words">
                       {item.head1}
                     </h1>
@@ -77,13 +77,13 @@ export function HeroBanner({ items }) {
                     )}
                   </div>
 
-                  <div className="w-full lg:col-span-9 xl:p-8 relative  h-[250px] md:h-[450px] lg:h-[450px] 2xl:h-[500px]   order-2 mt-8 lg:mt-2">
+                  <div className="w-full lg:col-span-8 xl:col-span-9 relative h-[250px] md:h-[450px] lg:h-[450px] order-2 mt-8 lg:mt-2">
                     <div className="w-full h-full relative">
                       <Image
                         src={item.image}
                         alt={`Slide ${index + 1}`}
                         fill
-                        className="object-contain md:object-cover"
+                        className="object-contain md:object-cover lg:object-contain"
                         priority={index === 0}
                         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 75vw, 66vw"
                         quality={90}
